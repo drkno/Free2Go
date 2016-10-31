8d993e945e1b1a5aad42edf2442c3633c0c477d0
Matthew <mrk45@uclive.ac.nz>
2016-10-31 18:53:21 +1300

    Minor Improvements

--- free2go.user.js
+++ free2go.user.js
@@ -3,9 +3,12 @@
 // @namespace   nz.co.makereti.free2goexploit
 // @description Give yourself discounts on any Mobi2Go orders
 // @include     *
-// @version     2.1
-// @icon        https://github.com/mrkno/Free2Go/raw/master/logo.png
+// @version     2.2
 // @grant       none
+// @run-at      document-end
+// @icon        https://github.com/mrkno/Free2Go/raw/master/logo.png
+// @downloadURL https://github.com/mrkno/Free2Go/raw/master/free2go.user.js
+// @updateURL   https://github.com/mrkno/Free2Go/raw/master/free2go.user.js
 // ==/UserScript==
 
 window.addEventListener('load', () => {
@@ -13,10 +16,10 @@ window.addEventListener('load', () => {
         return;
     }
     console.log('Mobi2Go Detected');
-	
+
 	let injection = '<div data-free-discount="" class="m2g-order-basket-section m2g-order-section m2g-order-section--complete"><div class="m2g-order-section-header"><span class="m2g-order-section-title">Discount</span><span class="m2g-order-section-status"></span></div><div class="" data-basket=""><style>.dcenter{text-align:center;}.dmaxWidthT{width:100%;margin:0;padding:0;}.dmaxWidthD{width:100%;text-align:center;}</style><div class="discountContainer"><table><tr><td class="dmaxWidthD"><input type="range" name="discountSlider" id="discountSlider" min="0.00" value="0.00" step="0.01" class="dmaxWidthT"></td><td class="dcenter"><span id="discountPercentage">100</span>&#37;</td></tr></table><table><tr><td><label for="discountAmount">$</label></td><td class="dmaxWidthD"><input id="discountAmount" name="discountAmount" type="number" min="0.00" step="0.01" value="0.00" placeholder="0.00" class="dmaxWidthT"></td></tr></table></div></div></div>';
 	$(injection).insertBefore('*[data-order-section="basket"]');
-	
+
 	let padLeft = (nr, n, str) => {
 		return Array(n-String(nr).length+1).join(str||'0')+nr;
 	};
@@ -33,15 +36,15 @@ window.addEventListener('load', () => {
 			return;
 		}
 		Mobi2Go.Order.setTip(-value);
-		
+
 		amount.css('border', '');
 		slider.css('border', '');
-		
+
 		if ($(event.target) != amount) amount.val(value);
 		if ($(event.target) != slider) slider.val(value);
 		percen.text(padLeft((100.0 * (value / total)).toFixed(0),3,' '));
 	};
-	
+
 	amount.on('input', updateDiscount);
 	slider.on('input', updateDiscount);
 
@@ -54,7 +57,7 @@ window.addEventListener('load', () => {
 		amount.val(discount);
 		slider.val(discount);
 		percen.text(padLeft((100.0 * (discount / total)).toFixed(0),3,' '));
-		
+
 		if (total <= 0) {
 			$('*[data-free-discount]').hide();
 		}
@@ -65,7 +68,7 @@ window.addEventListener('load', () => {
 
 	updateMax();
 	Mobi2Go.Order.bind('updateTotal', updateMax);
-	
+
 	let tipTotal = $('*[data-view="order-tip"]'),
 			inner = tipTotal.find('span');
 	tipTotal.text('Free');
